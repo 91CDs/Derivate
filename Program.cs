a1d2f9f19a7223e179e4cad1022da700a7651a18
C_D_91 <benamercarl@gmail.com>
2023-06-02 01:09:47 +0400

    added a Lexer

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace nineT1CD;
+
+public static class ToString
+{
+    public static string Repr(this List<Token> tokens)
+    {
+        return $"[ {String.Join(" , " , tokens.Select(t => t.ToString()))} ]";
+    }
+}
+
+public class Derivate
+{
+    public static void run(string input)
+    {
+        var Lexer = new Lexer(input);
+        var token = Lexer.parseText();
+        Console.WriteLine(token.Repr());
+    }
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("derivate > ");
+            string? input = Console.ReadLine();
+            if (input == String.Empty || input == null) break;
+            run(input);
+        }
+    }  
+}
