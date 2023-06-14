14b5cdb8658c687f35283d2df5636d1ee8786e4c
C_D_91 <110932534+91CDs@users.noreply.github.com>
2023-06-14 23:49:26 +0400

    added derivative functionality

--- Debug.cs
+++ Debug.cs
@@ -7,3 +7,84 @@ public static class Debug
         return $"[ { String.Join(" , ", tokens.Select(t => t.ToString())) } ]";
     }
 }
+
+public sealed class ASTPrint : NodeVisitor<string>
+{
+    public string print(Node expr)
+    {
+        return expr.accept<string>(this);
+    }
+    public string visitBinary(Binary node)
+    {
+        return $"( {print(node.left)} {node.operation} {print(node.right)} )";
+    }
+    public string visitUnary(Unary node)
+    {
+        return $"( {node.operation} {print(node.right)} )";
+    }
+    public string visitLiteral(Literal node)
+    {
+        return node.value.ToString()!;
+    }
+
+    public string visitGrouping(Grouping grouping)
+    {
+        return $"( g {print(grouping.expr)} )";
+    }
+}
+public sealed class FunctionPrint : NodeVisitor<string>
+{
+    Dictionary<TokenType, string> converter = 
+    new Dictionary<TokenType, string>()
+    {
+        {TokenType.SIN, "sin"},
+        {TokenType.COS, "cos"},
+        {TokenType.TAN, "tas"},
+        {TokenType.CSC, "csc"},
+        {TokenType.SEC, "sec"},
+        {TokenType.COT, "cot"},
+        {TokenType.LOG, "log"},
+        {TokenType.LN,  "ln"},
+        {TokenType.ADD, "+"},
+        {TokenType.SUB, "-"},
+        {TokenType.MUL, "*"},
+        {TokenType.DIV, "/"},
+        {TokenType.EXP, "^"}
+    };
+    public string print(Node expr)
+    {
+        return expr.accept<string>(this);
+    }
+    public string visitBinary(Binary node)
+    {
+        if (node.operation.type == TokenType.MUL)
+        {
+            bool isVar = node.right.getType() == TokenType.VAR;
+            bool isGrouping = node.left as Grouping != null || node.right as Grouping != null;
+
+            if (isVar || isGrouping)
+                return $"{print(node.left)}{print(node.right)}";
+        }
+
+        if (node.operation.type == TokenType.EXP)
+            return $"{print(node.left)}{converter.GetValueOrDefault(node.operation.type)}{print(node.right)}";
+
+        return $"{print(node.left)} {converter.GetValueOrDefault(node.operation.type)} {print(node.right)}";
+    }
+    public string visitUnary(Unary node)
+    {
+        if (node.operation.type == TokenType.SUB)
+            return $"-{print(node.right)}";
+
+        return $"{converter.GetValueOrDefault(node.operation.type)}({print(node.right)})";
+    }
+    public string visitLiteral(Literal node)
+    {
+        return node.value.ToString()!;
+    }
+
+    public string visitGrouping(Grouping node)
+    {
+        return $"({print(node.expr)})";
+    }
+}
